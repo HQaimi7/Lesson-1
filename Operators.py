6cd996afce277a8d2c31ea31524604bfbd1adcff
HQaimi7 <iqaimi@gmail.com>
2024-09-11 20:30:00 +0100

    Operators

---
+++ Operators.py
@@ -0,0 +1,11 @@
+#declaring variable
+x = 30
+y = 50
+sum = x + y
+diff = x - y
+mul = x * y
+div = x / y
+print("The sum of the numbers is ", sum)
+print("The difference between is ", diff)
+print("The product of the numbers is", mul)
+print("The Quotient of the numbers is", div)
