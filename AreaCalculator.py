9e87d83584638457706268b9a01e48b11768c55b
HQaimi7 <iqaimi@gmail.com>
2024-09-25 20:29:18 +0100

    Area Calculator

---
+++ AreaCalculator.py
@@ -0,0 +1,19 @@
+#area = pi * r * r
+#cir = 2 * pi * r
+
+r = float(input("Enter the radius of the circle: "))
+pi = 3.14159265359
+area = pi * r * r
+cir = 2 * pi * r
+print("Area = %0.2f" %area)
+print("Circumfrence = %0.2f" %cir)
+
+print("------------------------------------")
+
+side = float(input("Enter the size of the square: "))
+
+a = side * side
+perimeter = 4 * side
+
+print("Area = %0.2f" %a)
+print("Perimeter = %0.2f" %perimeter)
